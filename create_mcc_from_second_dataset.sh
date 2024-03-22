4648cb7359d9ad98dd8639740b1712b4383d4a63
richelbilderbeek <richel@richelbilderbeek.nl>
2024-03-22 09:05:14 +0100

    Can unzip

--- create_mcc_from_second_dataset.sh
+++ create_mcc_from_second_dataset.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget https://object.cloud.sdsc.edu/v1/AUTH_cipres/fares.kayakhi.5628/NGBW-JOB-BEAST_TG-444E59FE411D45A0949144F50A50E30C.zip
+# wget https://object.cloud.sdsc.edu/v1/AUTH_cipres/fares.kayakhi.5628/NGBW-JOB-BEAST_TG-444E59FE411D45A0949144F50A50E30C.zip
+unzip NGBW-JOB-BEAST_TG-444E59FE411D45A0949144F50A50E30C.zip
